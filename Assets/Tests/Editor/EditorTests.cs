eef8a34f2434a4c74a348a74c686627e0521b4cc
AdamKane <akane@forgefx.com>
2022-08-18 14:35:42 -1000

    fast pendar text

--- Assets/Tests/Editor/EditorTests.cs
+++ Assets/Tests/Editor/EditorTests.cs
@@ -11,6 +11,17 @@ public class EditorTests
 
 
     [Test]
+    [Category("Fast"), Category("Pendar_X10")]
+    public void PendarX10_Fast_EditMode_Test()
+    {
+        Console.WriteLine("Hello world, from fast test!");
+        var a = 2;
+        var b = 2;
+        var c = a + b;
+        Assert.AreEqual(expected: 4, actual: c);
+    }
+
+    [Test]
     [Category("Fast")]
     public void Fast_EditModeTest()
     {
